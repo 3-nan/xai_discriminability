7703f6672ed02f164b3ab70e2bb5a6e631d3d477
3-nan <43534318+3-nan@users.noreply.github.com>
2021-02-26 11:29:19 +0100

    Delete extract_results.sh

--- extract_results.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for filename in results/vgg16/cifar10/LRPAlpha1Beta0/*.tgz
-do
-  echo $filename
-  name=$(echo "$filename" | cut -f 1 -d '-')
-  mkdir "$name"
-  tar -C "$name"/ -xf $filename
-done
